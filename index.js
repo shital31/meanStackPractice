cfac39127fc9213ff33441d6625135d54c50691e
Shital Salunkhe <shital@4ibiz.in>
2023-12-07 17:25:53 +0530

    server setup

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const fs = require('fs');
+const http = require('http')
 
+//////////////////////////////////////
+// filesread write
+// const textIn =  fs.readFileSync('./txt/input.txt','utf-8')
+// console.log(textIn)
 
-const textIn =  fs.readFileSync('./txt/input.txt','utf-8')
-console.log(textIn)
+// const textOut = ` This is what we know about the avacado:${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut)
+// console.log('file written')
+/////////////////////////////////////////////////////////////////////////////
 
-const textOut = ` This is what we know about the avacado:${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut)
-console.log('file written')
+//erver request
+
+const server = http.createServer((req,res)=>{
+    res.end('Hello')
+})
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log("listning to server on port 8000")
+})
